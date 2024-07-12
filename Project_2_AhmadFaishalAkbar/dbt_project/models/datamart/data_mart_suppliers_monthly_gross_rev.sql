24df2988c169203346f9972e3cca1e2b710485a2
Ahmad Faishal A <113009001+ahmadfaishal9611@users.noreply.github.com>
2024-07-12 07:55:55 +0700

    Update data_mart_suppliers_monthly_gross_rev.sql

--- Project_2_AhmadFaishalAkbar/dbt_project/models/datamart/data_mart_suppliers_monthly_gross_rev.sql
+++ Project_2_AhmadFaishalAkbar/dbt_project/models/datamart/data_mart_suppliers_monthly_gross_rev.sql
@@ -2,6 +2,6 @@ SELECT
 	DATE_TRUNC('month', "orderDate") AS month,
 	"supplierCompany",
 	SUM("grossRevenue")	AS "grossRevenue"
-FROM {{ ref('dwh_order_details') }}
+FROM {{ ref('fact_order_details') }}
 GROUP BY month, "supplierCompany"
 ORDER BY month
